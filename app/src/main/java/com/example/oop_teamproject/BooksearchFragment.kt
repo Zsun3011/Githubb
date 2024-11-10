3840e5163d7985a0e52e79e6cbafdc0f240931ca
mhkqwwe1223 <mhkqwwe1223@naver.com>
2024-11-10 14:46:37 +0900

    오류 수정

--- app/src/main/java/com/example/oop_teamproject/BooksearchFragment.kt
+++ app/src/main/java/com/example/oop_teamproject/BooksearchFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [BookSearchFragment.newInstance] factory method to
+ * Use the [BooksearchFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class BookSearchFragment : Fragment() {
+class BooksearchFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class BookSearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_search, container, false)
+        return inflater.inflate(R.layout.fragment_booksearch, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class BookSearchFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment BookSearchFragment.
+         * @return A new instance of fragment BooksearchFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            BookSearchFragment().apply {
+            BooksearchFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
