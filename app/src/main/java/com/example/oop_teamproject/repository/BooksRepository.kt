7549f5a7e099517ba3633bda3c223652db0d20b7
Seek-99 <songic0308@gmail.com>
2024-11-26 04:19:17 +0900

    OOP_TP_v.0.6.6 :간단한 주석추가

--- app/src/main/java/com/example/oop_teamproject/repository/BooksRepository.kt
+++ app/src/main/java/com/example/oop_teamproject/repository/BooksRepository.kt
@@ -6,17 +6,17 @@ import kotlinx.coroutines.tasks.await
 
 class BooksRepository {
 
-    private val ref = FirebaseDatabase.getInstance().getReference("books")
+    private val ref = FirebaseDatabase.getInstance().getReference("books") //firebase에서 books 데이터 가져오기
 
     suspend fun getBooksByIds(bookIDs: List<String>): List<Book> {
         val snapshot = ref.get().await()
         val books = mutableListOf<Book>()
 
-        for (id in bookIDs) {
-            val bookSnapshot = snapshot.child(id)
+        for (id in bookIDs) { //프래그먼트의 bookIDs 참조
+            val bookSnapshot = snapshot.child(id) //해당 책 ID에 해당하는 데이터 가져오기
             val name = bookSnapshot.child("name").getValue(String::class.java) ?: "Unknown"
             val price = bookSnapshot.child("price").getValue(Int::class.java) ?: 0
-            books.add(Book(name, price))
+            books.add(Book(name, price)) //가져온 name과 price로 Book 객체 만들어 리스트 추가
         }
         return books
     }
