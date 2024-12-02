4864293e94b3171f9dcd5cdf7b6d2ac6e5ecd748
zsun <qkrwltjs3011@naver.com>
2024-12-03 00:29:39 +0900

    결제페이지에서 파이어베이스 저장

--- app/src/main/java/com/example/oop_teamproject/model/FileItem.kt
+++ app/src/main/java/com/example/oop_teamproject/model/FileItem.kt
@@ -5,6 +5,9 @@ class FileItem (
     val direction: String,    // 인쇄 방향 (가로, 세로)
     val page: String,         // 페이지 수
     val quantity: Int,        // 수량
-    val type: String,          // 인쇄 유형 (양면, 단면)
-    val name: String          // 파일 이름
+    val type: String,         // 인쇄 유형 (양면, 단면)
+    val name: String,         // 파일 이름
+    val date: String,         // 입력받은 날짜
+    val time: String,         // 입력받은 시간
+    val price: Int            // 결제 금액
 )
